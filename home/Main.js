072721bb7a79dbe9f0d9b945569f191aa9714260
Devhellom38 <104822733+Devhellom38@users.noreply.github.com>
2023-10-17 22:55:08 +0200

    Update Main.js

--- home/Main.js
+++ home/Main.js
@@ -1,12 +1,29 @@
-  function StartToggle() {
-        var projectBtns = document.getElementById('ProjectBtns');
-        var title = document.querySelector('.title');
+document.getElementById('SpecialSpan').addEventListener('click', function() {
+    StartToggle();
+});
 
-        if (projectBtns.style.display === 'none' || projectBtns.style.display === '') {
-            projectBtns.style.display = 'block';
-            title.style.display = 'none';
-        } else {
-            projectBtns.style.display = 'none';
-            title.style.display = 'block';
-        }
+function StartToggle() {
+    var projectBtns = document.getElementById('ProjectBtns');
+    var title = document.querySelector('.title');
+
+    if (projectBtns.style.display === 'none' || projectBtns.style.display === '') {
+        projectBtns.style.display = 'flex';
+        title.style.display = 'none';
+    } else {
+        projectBtns.style.display = 'none';
+        title.style.display = 'block';
     }
+}
+
+function StartToggleFalse() {
+    var projectBtns = document.getElementById('ProjectBtns');
+    var title = document.querySelector('.title');
+
+    if (projectBtns.style.display === 'none' || projectBtns.style.display === '') {
+        projectBtns.style.display = 'none';
+        title.style.display = 'none';
+    } else {
+        projectBtns.style.display = 'none';
+        title.style.display = 'block';
+    }
+}
